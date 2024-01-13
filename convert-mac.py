3b7f7d00f34841f72578336e8c6a78d017f32272
Michael Hubbard <michael.hubbard999@gmail.com>
2024-01-12 16:10:51 -0800

    Git Title: convert-mac.py
    Git Description:
    Formatting

--- convert-mac.py
+++ convert-mac.py
@@ -2,7 +2,7 @@ import argparse
 import re
 import sys
 
-'''
+"""
 Converts any format mac address to:
     Colon separated: 64:e8:81:43:cc:4e
     HPE format: 64e881-43cc4e
@@ -11,13 +11,14 @@ Converts any format mac address to:
     No Space Format: 64e88143cc4e
 
     Usage
-    convert-mac.py --mac 64e88143cc4e
+    convert-mac.py --mac 64:e8:81:43:cc:4e
     64:e8:81:43:cc:4e
     64e881-43cc4e
     64e8.8143.cc4e
     64-e8-81-43-cc-4e
     64e88143cc4e
-'''
+"""
+
 
 def format_mac(mac: str) -> str:
     """Converts most common MAC address formats
@@ -44,8 +45,8 @@ def format_mac(mac: str) -> str:
     Returns:
         str: MAC in aa:bb:cc:dd:ee:ff format
     """
-    mac = re.sub('[.:-]', '', mac).lower()  # remove delimiters, convert to lc
-    mac = ''.join(mac.split())  # remove whitespaces
+    mac = re.sub("[.:-]", "", mac).lower()  # remove delimiters, convert to lc
+    mac = "".join(mac.split())  # remove whitespaces
     assert len(mac) == 12  # length should be now exactly 12 (eg. 008041aefd7e)
     assert mac.isalnum()  # should only contain letters and numbers
     colon = mac
@@ -53,16 +54,16 @@ def format_mac(mac: str) -> str:
     cisco = mac
     ms = mac
     # convert mac in canonical form (eg. 00:80:41:ae:fd:7e)
-    colon = ":".join(["%s" % (mac[i:i+2]) for i in range(0, 12, 2)])
+    colon = ":".join(["%s" % (mac[i : i + 2]) for i in range(0, 12, 2)])
     # HPE format
-    hpe = "-".join(["%s" % (mac[i:i+6]) for i in range(0, 12, 6)])
+    hpe = "-".join(["%s" % (mac[i : i + 6]) for i in range(0, 12, 6)])
     # Cisco format
-    cisco = ".".join(["%s" % (mac[i:i+4]) for i in range(0, 12, 4)])
+    cisco = ".".join(["%s" % (mac[i : i + 4]) for i in range(0, 12, 4)])
     # MS format
-    ms = "-".join(["%s" % (mac[i:i+2]) for i in range(0, 12, 2)])
+    ms = "-".join(["%s" % (mac[i : i + 2]) for i in range(0, 12, 2)])
     # Build list of macs
     list1 = [colon, hpe, cisco, ms, mac]
-    MAC_Types = '\n'.join(list1)
+    MAC_Types = "\n".join(list1)
     return MAC_Types
 
 
@@ -72,7 +73,7 @@ args = parser.parse_args()
 mac = args.mac
 
 if mac is None:
-    print('-a mac address is a required argument')
+    print("-a mac address is a required argument")
     sys.exit()
 
 list_of_macs = format_mac(mac)
