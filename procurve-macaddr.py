b2418cda7932ce8deee08eba1dc73db4c7360106
Michael Hubbard <michael.hubbard999@gmail.com>
2024-01-25 14:27:37 -0800

    Git Title: procurve-macaddr.py
    Git Description:
    Initial commit of working version.

--- procurve-macaddr.py
+++ procurve-macaddr.py
@@ -1,4 +1,6 @@
 """
+# !!!!! Discovery Script - Does not change the running config !!!!!
+
 References:
 https://stackoverflow.com/questions/6545023/how-to-sort-ip-addresses-stored-in-dictionary-in-python/6545090#6545090
 https://stackoverflow.com/questions/20944483/python-3-sort-a-dict-by-its-values
@@ -82,7 +84,6 @@ it would use the last IP address.
 Clear the IP address in case the next interface has a MAC but no IP address
     IP_Data = ''
 """
-
 import argparse
 import hashlib
 import json
@@ -90,13 +91,21 @@ import os
 import re
 import sys
 
+from icecream import ic
+
 import manuf
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", type=str, required=False, help="File to read from")
-#   parser.add_argument('-o', '--output', type=str, help="Output file.")
-args = parser.parse_args()
+# ic.enable()
+ic.disable()
 
+__author__ = "Michael Hubbard"
+__author_email__ = "michael.hubbard999@gmail.com"
+__copyright__ = ""
+__license__ = "Unlicense"
+# -*- coding: utf-8 -*-
+#  procurve-macaddr.py
+#  Procurve Change Request data collection
+#  Created by Michael Hubbard on 2024-01-20.
 
 vernum = "1.0"
 AsciiArt = """
@@ -132,6 +141,18 @@ def version():
     print("+----------------------------------------------------------------------+")
 
 
+def remove_empty_lines(filename):
+    if not os.path.isfile(filename):
+        print("{} does not exist ".format(filename))
+        return
+    with open(filename) as filehandle:
+        lines = filehandle.readlines()
+
+    with open(filename, "w") as filehandle:
+        lines = filter(lambda x: x.strip(), lines)
+        filehandle.writelines(lines)
+
+
 def get_current_path(sub_dir1: str, extension: str = "", sub_dir2="") -> str:
     """
     returns a valid path regardless of the OS
@@ -150,154 +171,220 @@ def get_current_path(sub_dir1: str, extension: str = "", sub_dir2="") -> str:
     return int_report
 
 
-version()
 # MAC addresses are expressed differently depending on the manufacture and even model device
 # the formats that this script can parse are:
 # 0a:0a:0a:0a:0a:0a, 0a-0a-0a-0a-0a-0a, 0a0a0a.0a0a0a0 and 0a0a0a-0a0a0a
 # this should cover most Cisco and HP devices.
 #
-print()
-# create an empty dictionary to hold the mac-IP data
-Mac_IP = {}
-IP_Data = ""
-device_name = "Not Found"
-# create an empty list to hold MAC addresses for hashing
-hash_list = []
-# open the json created by arp.py if it exists
-my_json_file = "Mac2IP.json"
-try:
-    with open(my_json_file) as f:
-        Mac_IP = json.load(f)
-except FileNotFoundError as fnf_error:
-    print(fnf_error)
-    print("IP Addresses will not be included")
-    my_json_file = None
-p = manuf.MacParser()
-# create a blank list to accept each line in the file
-data = []
-# args.file contains the string entered after -f. That's becuase of the --file in the parser.add_argument function.
-if args.file:
-    mydatafile = args.file
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-f", "--file", type=str, required=False, help="File to read arp table from"
+)
+parser.add_argument("-s", "--site", help="Site name - ex. HQ")
+# parser.add_argument('-o', '--output', type=str, help="Output file.")
+args = parser.parse_args()
+site = args.site
+
+if site is None:
+    print("-s site name is a required argument")
+    sys.exit()
 else:
-    mydatafile = "mac-addr.txt"
-try:
-    f = open(mydatafile, "r")
-except FileNotFoundError as fnf_error:
-    print(fnf_error)
-    sys.exit(0)
-else:
-    for line in f:
-        match_PC = re.search(r"([0-9A-F]{2}[-:]){5}([0-9A-F]{2})", line, re.I)
-        match_Cisco = re.search(r"([0-9A-F]{4}[.]){2}([0-9A-F]{4})", line, re.I)
-        match_HP = re.search(r"([0-9A-F]{6}[-])([0-9A-F]{6})", line, re.I)
-        match_Interface = line.find("Port Address Table")
-        if match_PC or match_Cisco or match_HP or match_Interface != -1:
-            data.append(line)
-        device_name_loc = line.find("#")
-        if device_name_loc != -1:
-            device_name = line[0:device_name_loc]
-            device_name = device_name.strip()
-    f.close
-ct = len(data) - 1
-counter = 0
-IPs = []
-print()
-print("Device Name: %s " % device_name)
-print("PingInfo Data")
-while counter <= ct:
-    IP = data[counter]
-    # Remove newline at end
-    IP = IP.strip("\n")
-    # If the line contains Port Address Table
-    #    if line.find('Port Address Table'):
-    if IP.find("Port Address Table") != -1:
-        #   Remove the Status and Counters text.
-        IP = IP.replace("Status and Counters - ", "")
-        L = str.split(IP)
-        Interface_Num = L[4]
-        Vlan = ""
-        IP_Data = ""
-        Mac = ""
-        manufacture = ""
-    else:
-        L = str.split(IP)
-        Mac = L[0]
-        Vlan = L[1]
-        Vlan = Vlan.replace(",...", "")
-        temp = hash_list.append(Mac)
-        if Mac in Mac_IP:
-            IP_Data = Mac_IP[Mac]
-        else:
-            IP_Data = "No Match"
-        # print the pinginfo data
-        print(IP_Data, Mac)
-        # pull the manufacturer with manuf
-        manufacture = p.get_manuf(Mac)
-        # Pad with spaces for output alignment
-        Front_Pad = 4 - len(Vlan)
-        Pad = 7 - len(Vlan) - Front_Pad
-        Vlan = (" " * Front_Pad) + Vlan + (" " * Pad)
-        # Pad MAC Address Field
-        Pad = 22 - len(Mac)
-        Mac = Mac + (" " * Pad)
-        # Pad Interface
-        Pad = 8 - len(Interface_Num)
-        Interface_Num = Interface_Num + (" " * Pad)
-    # Pad IP address field if the json file exists
-    if my_json_file:
-        Pad = 17 - len(IP_Data)
-        IP_Data = IP_Data + (" " * Pad)
-        # create the separator at 80 characters
-        Pad = "--" * 35
-    else:
-        # if not create the separator at 60 characters since there won't be IPs
-        Pad = "--" * 25
-        IP_Data = ""
-    # Build the string
-    if IP.find("Port Address Table") != -1:
-        counter = counter + 1
-        continue
-    else:
-        IP = Vlan + IP_Data + Mac + Interface_Num + str(manufacture)
-        IPs.append(str(IP))
-        IPs.append(Pad)
-    # Clear the IP address in case the next interface has a MAC but no IP address
+    dev_inv_file = f"device-inventory-{site}.csv"
+
+ic(dev_inv_file)
+# check if site's device inventory file exists
+if not os.path.isfile(dev_inv_file):
+    print("{} doesn't exist ".format(dev_inv_file))
+    sys.exit()
+
+remove_empty_lines(dev_inv_file)
+
+with open(dev_inv_file) as devices_file:
+    fabric = devices_file.readlines()
+
+print("-" * (len(dev_inv_file) + 23))
+print(f"Reading devices from: {dev_inv_file}")
+print("-" * (len(dev_inv_file) + 23))
+for line in fabric:
+    line = line.strip("\n")
+    hostname = line.split(",")[2]
+
+    mac_file = get_current_path("port-maps", "-mac-address.txt", "data")
+    ic(mac_file)
+
+    print()
+    # create an empty dictionary to hold the mac-IP data
+    Mac_IP = {}
     IP_Data = ""
-    counter = counter + 1
-d = int(len(IPs) / 2)
-print()
-print("Number of Entries: %s " % d)
-print()
-print("Device Name: %s " % device_name)
-if my_json_file:
-    print("Vlan   IP Address       MAC Address       Interface   Vendor")
-    print("--" * 35)
-else:
-    print("Vlan   MAC Address       Interface   Vendor")
-    print("--" * 25)
-for IP in IPs:
-    print(IP)
+    device_name = "Not Found"
+    # create an empty list to hold MAC addresses for hashing
+    hash_list = []
+    # open the json created by arp.py if it exists
+    my_json_file = get_current_path("port-maps", "-Mac2IP.json", "data")
+    try:
+        with open(my_json_file) as f:
+            Mac_IP = json.load(f)
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+        print("IP Addresses will not be included because Mac2IP.json is not available")
+        my_json_file = None
+    p = manuf.MacParser()
 
-"""
-hash the string of all macs. This gives a quick way to compare the
-before and after MACS
-"""
+    data = []
+    # args.file contains the string entered after -f. That's because of the --file in the parser.add_argument function.
+    # --------- shouldn't be necessary ------
+    # if args.file:
+    #     mydatafile = args.file
+    # else:
+    #     mydatafile = "mac-addr.txt"
+    # ---------------------------------------
 
-hash_list_str = str(hash_list)
-# convert the string to bytes
-b = hash_list_str.encode()
-hash_object = hashlib.md5(b)
-print()
-print("Hash of all the MAC addresses")
-print(hash_object.hexdigest())
-print()
+    try:
+        f = open(mac_file, "r")
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+        sys.exit(0)
+    else:
+        for line in f:
+            match_PC = re.search(r"([0-9A-F]{2}[-:]){5}([0-9A-F]{2})", line, re.I)
+            match_Cisco = re.search(r"([0-9A-F]{4}[.]){2}([0-9A-F]{4})", line, re.I)
+            match_HP = re.search(r"([0-9A-F]{6}[-])([0-9A-F]{6})", line, re.I)
+            match_Interface = line.find("Port Address Table")
+            if match_PC or match_Cisco or match_HP or match_Interface != -1:
+                data.append(line)
+            device_name_loc = line.find("#")
+            if device_name_loc != -1:
+                device_name = line[0:device_name_loc]
+                device_name = device_name.strip()
+        f.close
+    ct = len(data) - 1
+    counter = 0
+    IPs = []
+    print()
+    print("Device Name: %s " % device_name)
+    print("PingInfo Data")
+    while counter <= ct:
+        IP = data[counter]
+        # Remove newline at end
+        IP = IP.strip("\n")
 
-"""
-print out the MAC Addresses sorted for review.
-This is useful if the patch cables got mixed up during replacement
-"""
-print("Sorted list of MAC Addresses")
-# print(hash_list)
-for x in sorted(hash_list):
-    print(x)
-print("End of output")
+        if IP.find("Port Address Table") != -1:
+            #   Remove the Status and Counters text.
+            IP = IP.replace("Status and Counters - ", "")
+            L = str.split(IP)
+            Interface_Num = L[4]
+            Vlan = ""
+            IP_Data = ""
+            Mac = ""
+            manufacture = ""
+        else:
+            L = str.split(IP)
+            Mac = L[0]
+            Vlan = L[1]
+            Vlan = Vlan.replace(",...", "")
+            temp = hash_list.append(Mac)
+            if Mac in Mac_IP:
+                IP_Data = Mac_IP[Mac]
+            else:
+                IP_Data = "No Match"
+            # print the pinginfo data
+            print(IP_Data, Mac)
+            # pull the manufacturer with manuf
+            manufacture = p.get_manuf(Mac)
+            # Pad with spaces for output alignment
+            Front_Pad = 4 - len(Vlan)
+            Pad = 7 - len(Vlan) - Front_Pad
+            Vlan = (" " * Front_Pad) + Vlan + (" " * Pad)
+            # Pad MAC Address Field
+            Pad = 22 - len(Mac)
+            Mac = Mac + (" " * Pad)
+            # Pad Interface
+            Pad = 8 - len(Interface_Num)
+            Interface_Num = Interface_Num + (" " * Pad)
+        # Pad IP address field if the json file exists
+        if my_json_file:
+            Pad = 17 - len(IP_Data)
+            IP_Data = IP_Data + (" " * Pad)
+            # create the separator at 80 characters
+            Pad = "--" * 35
+        else:
+            # if not create the separator at 60 characters since there won't be IPs
+            Pad = "--" * 25
+            IP_Data = ""
+        # Build the string
+        if IP.find("Port Address Table") != -1:
+            counter = counter + 1
+            continue
+        else:
+            IP = Vlan + IP_Data + Mac + Interface_Num + str(manufacture)
+            IPs.append(str(IP))
+            IPs.append(Pad)
+        # Clear the IP address in case the next interface has a MAC but no IP address
+        IP_Data = ""
+        counter = counter + 1
+    d = int(len(IPs) / 2)
+
+    output_file = get_current_path("port-maps", "-ports.txt", "Final")
+    ic(output_file)
+
+    with open(output_file, "w") as f:
+        sys.stdout = f
+
+        # Redirect print statements to the file
+        version()
+        print()
+        print(f"Number of Entries: {d}")
+        print()
+        print(f"Device Name: {device_name}")
+        if my_json_file:
+            header = "Vlan   IP Address       MAC Address       Interface   Vendor"
+            print("Vlan   IP Address       MAC Address       Interface   Vendor")
+            print("--" * 40)
+        else:
+            header = "Vlan   MAC Address       Interface   Vendor"
+            print("Vlan   MAC Address       Interface   Vendor")
+            print("--" * 30)
+        for IP in IPs:
+            print(IP)
+
+    # Reset print function to default
+    sys.stdout = sys.__stdout__
+
+    print()
+    print("Number of Entries: %s " % d)
+    print()
+    print("Device Name: %s " % device_name)
+    if my_json_file:
+        print("Vlan   IP Address       MAC Address       Interface   Vendor")
+        print("--" * 35)
+    else:
+        print("Vlan   MAC Address       Interface   Vendor")
+        print("--" * 25)
+    for IP in IPs:
+        print(IP)
+
+    """
+    hash the string of all macs. This gives a quick way to compare the
+    before and after MACS
+    """
+
+    hash_list_str = str(hash_list)
+    # convert the string to bytes
+    b = hash_list_str.encode()
+    hash_object = hashlib.md5(b)
+    print()
+    print("Hash of all the MAC addresses")
+    print(hash_object.hexdigest())
+    print()
+
+    """
+    print out the MAC Addresses sorted for review.
+    This is useful if the patch cables got mixed up during replacement
+    """
+    print("Sorted list of MAC Addresses")
+    # print(hash_list)
+    for x in sorted(hash_list):
+        print(x)
+    print("End of output")
