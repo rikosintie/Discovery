902e40117c48e38997824f44124038df746579ba
rikosintie <michael.hubbard999@gmail.com>
2024-11-04 08:27:22 -0800

    Git Title: filenames.py
    Git Description:
    Updated to read each project data directory and save a csv
    and excel file in the directory.

--- filenames.py
+++ filenames.py
@@ -1,10 +1,11 @@
 import csv
 import os
+from pathlib import Path
 
 import pandas as pd
 
-# set directory_path to the directory you want to read.
-# reads all the files in the directory and saves the names to excel, csv.
+# reads all the files in the collection directories and saves the names to
+# an excel, and csv file.
 
 
 def get_filenames(directory_path):
@@ -13,10 +14,29 @@ def get_filenames(directory_path):
     return filenames
 
 
-def save_to_csv(filename, data):
+# def save_to_excel(filename, data):
+#     df = pd.DataFrame(data, columns=["File Name"])
+#     df.to_excel(filename, index=False)
+
+
+def get_current_path():
+    current_path = os.getcwd()
+    # current_path = os.path.join(current_path, "Running")
+
+    return current_path
+
+
+def save_to_excel(folder, filename, data):
     loc = get_current_path()
-    # loc = loc + '\\running\\'
-    filename = loc + "/" + filename
+    filename: str = os.path.join(loc, folder, filename)
+    df = pd.DataFrame(data, columns=["File Name"])
+    df.to_excel(filename, index=False)
+
+
+def save_to_csv(folder, filename, data):
+    loc = get_current_path()
+    filename: str = os.path.join(loc, folder, filename)
+
     with open(filename, "w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["File Name"])
@@ -24,29 +44,41 @@ def save_to_csv(filename, data):
             csv_writer.writerow([row])
 
 
-def save_to_excel(filename, data):
-    df = pd.DataFrame(data, columns=["File Name"])
-    df.to_excel(filename, index=False)
+def main():
+    proj_directories = [
+        "CR-data",
+        "Interface",
+        "port-maps/Final",
+        "Running",
+    ]
 
+    for folder in proj_directories:
+        # directory_path = "/home/mhubbard/Insync/michael.hubbard999@gmail.com/GoogleDrive/04_Tools/Discovery/Running/"  # Replace this with the actual directory path
+        directory_path = get_current_path()
+        directory_path = os.path.join(directory_path, folder)
+        # Get the filenames from the directorycurrent_path = os.path.join(current_path, "Running")
+        filenames = get_filenames(directory_path)
 
-def get_current_path():
-    current_path = os.getcwd()
-    return current_path
+        # Save to CSV
+        # The port-maps folder stores the results in a subdirectory Final.
+        # This removes the "/" and -Final from the name.
+        if "/" in folder:
+            csv_filename = folder.split("/")[0] + ".csv"
+        else:
+            csv_filename = folder + ".csv"
+
+        save_to_csv(folder, csv_filename, filenames)
+
+        # Save to Excel
+        # This removes the "/" and -Final from the name.
+        if "/" in folder:
+            excel_filename = folder.split("/")[0] + ".xlsx"
+        else:
+            excel_filename = folder + ".xlsx"
+        save_to_excel(folder, excel_filename, filenames)
+
+    print("Filenames have been saved to CSV and Excel files.")
 
 
 if __name__ == "__main__":
-    directory_path = "/home/mhubbard/Insync/michael.hubbard999@gmail.com/GoogleDrive/04_Tools/Discovery/Running/"  # Replace this with the actual directory path
-    # directory_path = get_current_path()
-
-    # Get the filenames from the directory
-    filenames = get_filenames(directory_path)
-
-    # Save to CSV
-    csv_filename = "filenames.csv"
-    save_to_csv(csv_filename, filenames)
-
-    # Save to Excel
-    excel_filename = "filenames.xlsx"
-    save_to_excel(excel_filename, filenames)
-
-    print("Filenames have been saved to CSV and Excel files.")
+    main()
